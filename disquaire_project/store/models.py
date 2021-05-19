4794ca34207822fb52372da7d7339b47ae0c98b5
Eric Dasse <christ.gououi@gmail.com>
2021-05-19 19:17:41 +0200

    Add artists and albums mockup models

--- disquaire_project/store/models.py
+++ disquaire_project/store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+ARTISTS = {
+    'francis-cabrel': {'name': 'Francis Cabrel'},
+    'lej': {'name': 'Elijay'},
+    'rosana': {'name': 'Rosanna'},
+    'maria-dolores-pradera': {'name': 'Maria Dolores Pradera'},
+}
+
+ALBUMS = [
+    {'name': 'Sarcabane', 'artists': [ARTISTS['francis-cabrel']]},
+    {'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
+    {'name': 'Luna Nueva', 'artists': [ARTISTS['rosana'], ARTISTS['maria-dolores-pradera']]}
+]

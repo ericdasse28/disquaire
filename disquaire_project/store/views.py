fd7b24ad2fd526bc6f77c79e4595145a28b9896c
Eric Dasse <christ.gououi@gmail.com>
2021-05-19 20:20:25 +0200

    Add a view function to represent an album

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from .models import ALBUMS
+from .models import ALBUMS, ARTISTS
 
 
 def index(request):
@@ -10,5 +10,15 @@ def index(request):
 def listing(request):
     albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
     message = """<ul>{}</ul>""".format("\n".join(albums))
+
+    return HttpResponse(message)
+
+
+def detail(request, album_id):
+    a_id = int(album_id)  # Make sure we have an integer
+    album = ALBUMS[a_id]  # Get the album with its id
+    artists = " ".join([artist['name'] for artist in ARTISTS])  # Grab artists name and
+    # create a string out of it.
+    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album['name'], artists)
 
     return HttpResponse(message)

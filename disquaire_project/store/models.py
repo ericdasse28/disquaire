7d3891080905a5277df75c99cc2222d701a1e2ca
Eric Dasse <christ.gououi@gmail.com>
2021-06-14 15:44:35 +0200

    Add many to many relation in Album model

--- disquaire_project/store/models.py
+++ disquaire_project/store/models.py
@@ -16,6 +16,7 @@ class Album(models.Model):
     available = models.BooleanField(default=True)
     title = models.CharField(max_length=200)
     picture = models.URLField()
+    artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
 
 
 class Booking(models.Model):

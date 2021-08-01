86ede3f0a1d5c48e3d64bede79b2bb58dfa90da1
Eric Dasse <christ.gououi@gmail.com>
2021-08-01 16:06:32 +0200

    Feat index view using Album model

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
-# from .models import ALBUMS
+from .models import Album, Artist, Contact, Booking
 
 
 def index(request):
-    message = "Salut tout le monde !"
+    albums = Album.objects.filter(available=True).order_by('created_at')[:12]
+    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
+    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
     return HttpResponse(message)
 
 

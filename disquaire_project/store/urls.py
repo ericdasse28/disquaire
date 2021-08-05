4ed4358d97477f890cf1a62e119fade8aa873dc4
Eric Dasse <christ.gououi@gmail.com>
2021-08-06 01:05:53 +0200

    Feat names for store url paths

--- disquaire_project/store/urls.py
+++ disquaire_project/store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listing),  # "/store" will call the method "index" in "views.py"
-    path('<int:album_id>', views.detail),
-    path('search/', views.search)
+    path('', views.listing, name='listing'),  # "/store" will call the method "index" in "views.py"
+    path('<int:album_id>', views.detail, name='detail'),
+    path('search/', views.search, name='search')
 ]

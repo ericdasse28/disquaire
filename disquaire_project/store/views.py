8b4d5cbd5a251401b46d2c59701cc85b6c0df81f
Eric Dasse <christ.gououi@gmail.com>
2021-08-06 13:20:28 +0200

    Feat exceptions for listing view

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from .models import Album, Artist, Contact, Booking
 
@@ -15,9 +16,20 @@ def index(request):
 
 
 def listing(request):
-    albums = Album.objects.filter(available=True)
-    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
-    message = """<ul>{}<ul>""".format("\n".join(formatted_albums))
+    albums_list = Album.objects.filter(available=True)
+    # Slice pages
+    paginator = Paginator(albums_list, 9)
+    # Get the current page number
+    page = request.GET.get('page')
+    # Return only this page albums and not others
+    try:
+        albums = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page
+        albums = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        albums = paginator.page(paginator.num_pages)
 
     context = {
         'albums': albums

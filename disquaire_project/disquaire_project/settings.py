65ded14174990de107ce330389ec06d234a8538a
Eric Dasse <christ.gououi@gmail.com>
2021-08-06 11:13:40 +0200

    Deactivate debug mode

--- disquaire_project/disquaire_project/settings.py
+++ disquaire_project/disquaire_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6k26xnq%!l4q*y_bw&qv3d78y6$n-g7)g2n$vbqurv)--mymm0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition

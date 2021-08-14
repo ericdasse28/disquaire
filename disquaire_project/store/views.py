24f8627fdd04ad13f3179d46564d48cac472e9f8
Eric Dasse <christ.gououi@gmail.com>
2021-08-14 11:43:22 +0200

    Replay: detail view in views.py

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -38,8 +38,8 @@ def listing(request):
 
 def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
-    artists = [artist.name for artist in album.artists.all()]
-    artists_name = "\t".join(artists)
+    artists_name = " ".join([artist for artist in album.artists.all()])
+
     context = {
         'album_title': album.title,
         'artists_name': artists_name,
@@ -47,43 +47,6 @@ def detail(request, album_id):
         'thumbnail': album.picture
     }
 
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            name = form.cleaned_data['name']
-
-            contact = Contact.objects.filter(email=email)
-            if not contact.exists():
-                # If a contact is not registered, create a new one
-                contact = Contact.objects.create(
-                    email=email,
-                    name=name
-                )
-            else:
-                contact = contact.first()
-
-            album = get_object_or_404(Album, id=album_id)
-            booking = Booking.objects.create(
-                contact=contact,
-                album=album
-            )
-
-            album.available = False
-            album.save()
-            context = {
-                'album_title': album.title
-            }
-
-            return render(request, 'store/merci.html', context)
-        else:
-            # Form data doesn't match the expected format
-            # Add errors to the template
-            context['errors'] = form.errors.items()
-    else:
-        form = ContactForm()
-
-    context['form'] = form
     return render(request, 'store/detail.html', context)
 
 

aed88f1de0cde74cb43228eaa77c19b22f7dfada
Eric Dasse <christ.gououi@gmail.com>
2021-08-14 13:35:52 +0200

    Replat form with ModelForm

--- disquaire_project/store/forms.py
+++ disquaire_project/store/forms.py
@@ -1,16 +1,7 @@
 from django.forms import ModelForm, TextInput, EmailInput
 from django.forms.utils import ErrorList
+
 from .models import Contact
-
-
-class ContactForm(ModelForm):
-    class Meta:
-        model = Contact
-        fields = ["name", "email"]
-        widgets = {
-            'name': TextInput(attrs={'class': 'form-control'}),
-            'email': EmailInput(attrs={'class': 'form-control'})
-        }
 
 
 class ParagraphErrorList(ErrorList):
@@ -21,3 +12,13 @@ class ParagraphErrorList(ErrorList):
         if not self:
             return ''
         return '<div class="errorlist">{}</div>'.format(''.join([f'<p class="small error">{e}</p>' for e in self]))
+
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields = ["name", "email"]
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'email': EmailInput(attrs={'class': 'form-control'})
+        }

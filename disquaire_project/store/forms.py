f6e0eea5a6a47d42d7792a8b55b5e1443fe8a6c7
Eric Dasse <christ.gououi@gmail.com>
2021-08-14 13:11:25 +0200

    Feat paragraph error list class

--- disquaire_project/store/forms.py
+++ disquaire_project/store/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm, TextInput, EmailInput
+from django.forms.utils import ErrorList
 from .models import Contact
 
 
@@ -10,3 +11,13 @@ class ContactForm(ModelForm):
             'name': TextInput(attrs={'class': 'form-control'}),
             'email': EmailInput(attrs={'class': 'form-control'})
         }
+
+
+class ParagraphErrorList(ErrorList):
+    def __str__(self):
+        return self.as_divs()
+
+    def as_divs(self):
+        if not self:
+            return ''
+        return '<div class="errorlist">{}</div>'.format(''.join([f'<p class="small error">{e}</p>' for e in self]))
